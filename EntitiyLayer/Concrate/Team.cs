01d1687c2b08281c7166e479b72f561fafdc195d
Bengü Çağla SARI <81232710+bengucagla@users.noreply.github.com>
2023-05-30 20:45:39 +0300

    Add files via upload

---
+++ EntitiyLayer/Concrate/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyLayer.Concrate
+{
+	public class Team
+	{
+		[Key]
+		public int TeamID { get; set; }
+		public string TeamName { get; set; }
+		public virtual ICollection<Match> HomeMatches { get; set; }
+		public virtual ICollection<Match> AwayMatches { get; set; }
+	}
+}
