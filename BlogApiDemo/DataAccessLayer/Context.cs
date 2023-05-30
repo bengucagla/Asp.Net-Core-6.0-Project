a309952c699d17ece1256c8e8f56343b2df7e66c
Bengü Çağla SARI <81232710+bengucagla@users.noreply.github.com>
2023-05-30 20:37:33 +0300

    Add files via upload

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-GOIG5JI;Database=mydatabaseApi;User Id=web;Password=web987;TrustServerCertificate=True;");
+
+
+		}
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
